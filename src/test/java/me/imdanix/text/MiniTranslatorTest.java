1b1f9f7be0edbf4e7cc0c5a887a7fdf78c7347ad
Daniil Z. <idanix@list.ru>
2025-05-28 03:43:34 +0400

    Simplify tests

--- src/test/java/me/imdanix/text/MiniTranslatorTest.java
+++ src/test/java/me/imdanix/text/MiniTranslatorTest.java
@@ -18,41 +18,62 @@ public class MiniTranslatorTest {
         return new Object[][] {
                 {
                         "&aA simple one",
-                        "<green>A simple one"
+                        "<green>A simple one",
+                        "<green>A simple one</green>"
                 }, {
                         "F&#123456oo &x&6&5&4&3&2&1ba#12345r",
-                        "F<#123456>oo <#654321>ba#12345r"
+                        "F<#123456>oo <#654321>ba#12345r",
+                        "F<color:#123456>oo </color:#123456><color:#654321>ba#12345r</color:#654321>"
                 }, {
                         "&@red-yellow-0@&lServer admin &9imDaniX &8> &#fff5d9&oHello world! YOLO",
-                        "<gradient:red:yellow:black><b>Server admin <reset><blue>imDaniX <dark_gray>> <#fff5d9><i>Hello world! YOLO"
+                        "<gradient:red:yellow:black><b>Server admin <reset><blue>imDaniX <dark_gray>> <#fff5d9><i>Hello world! YOLO",
+                        "<gradient:red:yellow:black><b>Server admin </b></gradient><blue>imDaniX </blue><dark_gray>> </dark_gray><color:#fff5d9><i>Hello world! YOLO</i></color:#fff5d9>"
                 }, {
                         "&a&lGreen bold, &cred normal",
-                        "<green><b>Green bold, <reset><red>red normal"
+                        "<green><b>Green bold, <reset><red>red normal",
+                        "<green><b>Green bold, </b></green><red>red normal</red>"
                 }, {
                         "&lBold &athen green",
-                        "<b>Bold </b><green>then green"
+                        "<b>Bold </b><green>then green",
+                        "<b>Bold </b><green>then green</green>"
                 }, {
                         "§lJust bold",
-                        "<b>Just bold"
+                        "<b>Just bold",
+                        "<b>Just bold</b>"
                 }, {
                         "&FHOW TO TURN OFF &C&LCAPS LOCK&#123ABC?!",
                         "<white>HOW TO TURN OFF <red><b>CAPS LOCK<reset><#123ABC>?!",
+                        "<white>HOW TO TURN OFF </white><red><b>CAPS LOCK</b></red><color:#123ABC>?!</color:#123ABC>"
                 }, {
+                        "Invalid &jcolor",
                         "Invalid &jcolor",
                         "Invalid &jcolor"
                 }, {
                         "&a&lStart from the&r scratch",
+                        "<green><b>Start from the<reset> scratch",
                         "<green><b>Start from the<reset> scratch"
                 }, {
                         "&eYellow, &&astill yellow",
-                        "<yellow>Yellow, &astill yellow"
+                        "<yellow>Yellow, &astill yellow",
+                        "<yellow>Yellow, &astill yellow</yellow>"
                 }
         };
     }
 
     @Test(dataProvider = "toMiniData")
-    public void toMiniTest(String input, String expected) {
+    public void toMiniTest(String input, String expected, String ignored) {
         assertEquals(MiniTranslator.toMini(input), expected);
+    }
+
+    private static final Set<MiniTranslator.Option> VERBOSE = EnumSet.allOf(MiniTranslator.Option.class);
+    static {
+        VERBOSE.remove(MiniTranslator.Option.FAST_RESET);
+        VERBOSE.remove(MiniTranslator.Option.HEX_COLOR_STANDALONE);
+    }
+
+    @Test(dataProvider = "toMiniData")
+    public void toMiniVerboseTest(String input, String ignored, String expected) {
+        assertEquals(MiniTranslator.toMini(input, VERBOSE), expected);
     }
 
     @DataProvider
@@ -84,54 +105,6 @@ public class MiniTranslatorTest {
         assertEquals(MiniTranslator.toMini(input, STANDALONE), expected);
     }
 
-    private static final Set<MiniTranslator.Option> VERBOSE = EnumSet.allOf(MiniTranslator.Option.class);
-    static {
-        VERBOSE.remove(MiniTranslator.Option.FAST_RESET);
-        VERBOSE.remove(MiniTranslator.Option.HEX_COLOR_STANDALONE);
-    }
-
-    @DataProvider
-    public Object[][] toMiniVerboseData() {
-        return new Object[][] {
-                {
-                        "&aA simple one",
-                        "<green>A simple one</green>"
-                }, {
-                        "F&#123456oo &x&6&5&4&3&2&1ba#12345r",
-                        "F<color:#123456>oo </color:#123456><color:#654321>ba#12345r</color:#654321>"
-                }, {
-                        "&@red-yellow-0@&lServer admin &9imDaniX &8> &#fff5d9&oHello world! YOLO",
-                        "<gradient:red:yellow:black><b>Server admin </b></gradient><blue>imDaniX </blue><dark_gray>> </dark_gray><color:#fff5d9><i>Hello world! YOLO</i></color:#fff5d9>"
-                }, {
-                        "&a&lGreen bold, &cred normal",
-                        "<green><b>Green bold, </b></green><red>red normal</red>"
-                }, {
-                        "&lBold &athen green",
-                        "<b>Bold </b><green>then green</green>"
-                }, {
-                        "§lJust bold",
-                        "<b>Just bold</b>"
-                }, {
-                        "&FHOW TO TURN OFF &C&LCAPS LOCK&#123ABC?!",
-                        "<white>HOW TO TURN OFF </white><red><b>CAPS LOCK</b></red><color:#123ABC>?!</color:#123ABC>",
-                }, {
-                        "Invalid &jcolor",
-                        "Invalid &jcolor"
-                }, {
-                        "&a&lStart from the&r scratch",
-                        "<green><b>Start from the<reset> scratch"
-                }, {
-                        "&eYellow, &&astill yellow",
-                        "<yellow>Yellow, &astill yellow</yellow>"
-                }
-        };
-    }
-
-    @Test(dataProvider = "toMiniVerboseData")
-    public void toMiniVerboseTest(String input, String expected) {
-        assertEquals(MiniTranslator.toMini(input, VERBOSE), expected);
-    }
-
     private static final String SYMBOLS = "&@-02468#abcdef";
     private static final Set<MiniTranslator.Option> VERBOSE_STANDALONE = EnumSet.copyOf(VERBOSE);
     static {
@@ -140,10 +113,11 @@ public class MiniTranslatorTest {
 
     @Test(description = "Basically hoping that we'll get no exceptions while parsing a hot stinky mess")
     public void randomTest() {
+        int length = 512;
         RandomGenerator rng = ThreadLocalRandom.current();
         for (int i = 0; i < 1024; i++) {
-            StringBuilder builder = new StringBuilder(256);
-            for (int j = 0; j < 512; j++) {
+            StringBuilder builder = new StringBuilder(length);
+            for (int j = 0; j < length; j++) {
                 builder.append(SYMBOLS.charAt(rng.nextInt(SYMBOLS.length())));
             }
             try {
